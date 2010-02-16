3c96c9a4e6287ca69032b3486794dab28e1b1804
Zachary Dovel <pizzach@gmail.com>
2010-02-15 22:02:42 -0500

    Added " " to the ignore character list when calulating the type of query.  "decimal point" was not being interpreted as a romaji query which caused it to return no results.

--- src/utilities.c
+++ src/utilities.c
@@ -252,6 +252,7 @@ gboolean gw_util_all_chars_are_in_range (char input[],
                                                                                    || unic == ']'
                                                                                    || unic == '&'
                                                                                    || unic == '|'
+                                                                                   || unic == ' '
                                                                                    || unic == '('
                                                                                    || unic == ')');
     }
