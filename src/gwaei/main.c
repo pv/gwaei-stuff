21442596ce7136248c29ee0b7f3093a360ee76e0
Zachary Dovel <pizzach@gmail.com>
2011-07-30 16:30:31 -0400

    Set the priority of gw_main_keep_searching_timeout to G_PRIORITY_DEFAULT_IDLE from G_PRIORITY_LOW for wine because the function would stop being called until the next search.

--- src/gwaei/main.c
+++ src/gwaei/main.c
@@ -2195,7 +2195,9 @@ gboolean gw_main_keep_searching_timeout (gpointer data)
     window = GTK_WIDGET (gtk_builder_get_object (builder, "settings_window"));
     
     if (gtk_widget_get_visible (window) == FALSE && gw_settings_get_search_as_you_type ())
+    {
       gw_main_search_cb (NULL, NULL);
+    }
 
     return TRUE;
 }
